1a78f55191844b9f4d436e2eaff94bc4a6bb8f03
Tan Nha <huynhtannha2003@gmail.com>
2024-08-23 07:57:51 +0700

    Week1_JS_Ex1 BMI

---
+++ index.js
@@ -0,0 +1,27 @@
+var Marksmass = 78
+var Marksheight = 1.69
+var BMIMarks = Marksmass / Marksheight ** 2
+console.log(BMIMarks);
+
+var Johnmass = 92
+var Johnheight = 1.95
+var BMIJohn = Johnmass / Johnheight ** 2
+console.log(BMIJohn);
+markHigherBMI = BMIMarks > BMIJohn
+console.log("BMIMarks: ", BMIMarks);
+console.log("BMIJohn: ", BMIJohn);
+console.log("Marks higher John: ", markHigherBMI);
+
+var Marksmass = 95
+var Marksheight = 1.88
+var BMIMarks = Marksmass / Marksheight ** 2
+console.log(BMIMarks);
+
+var Johnmass = 85
+var Johnheight = 1.76
+var BMIJohn = Johnmass / Johnheight ** 2
+console.log(BMIJohn);
+markHigherBMI = BMIMarks > BMIJohn
+console.log("BMIMarks: ", BMIMarks);
+console.log("BMIJohn: ", BMIJohn);
+console.log("John higher Marks: ", markHigherBMI);
